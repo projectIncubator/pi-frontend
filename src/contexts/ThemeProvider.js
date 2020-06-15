e8e0702e4e57d77080a8bf9b77f3148fe02fdf97
Hal Shin <hal.shin@alumni.ubc.ca>
2020-06-15 16:58:43 -0700

    Add local storage hook, add darkmode setting to local storage

--- src/contexts/ThemeProvider.js
+++ src/contexts/ThemeProvider.js
@@ -5,6 +5,8 @@ import {
   MuiThemeProvider,
   responsiveFontSizes
 } from '@material-ui/core/styles';
+
+import { useLocalStorageState } from '../hooks';
 import { lightTheme, darkTheme } from '../themes';
 
 const light = responsiveFontSizes(createMuiTheme(lightTheme));
@@ -13,7 +15,7 @@ const dark = responsiveFontSizes(createMuiTheme(darkTheme));
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 import { useAuth0 } from '../contexts/AuthProvider';
+import { useLocalStorageState } from './useLocalStorageState';
 
-export { useAuth0 as useAuth };
+export { useAuth0 as useAuth, useLocalStorageState };
